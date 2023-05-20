b519bc10d2a683ceb6a33f18b7cf31ad67fc0589
setali <set.ali.mousavi@gmail.com>
2023-05-20 15:56:31 +0330

    Session 3

---
+++ index.js
@@ -0,0 +1,185 @@
+//  ! @ # $ % ^ & * () _ - + = \ / | [] {} ~ ` ' " : ; ? . ,
+
+
+
+// var obj = {
+//   name: 'Ali',
+//   family: 'Mousavi',
+//   fullName: 'Ali Mousavi'
+// }
+
+// console.log(obj.fullName || obj.family || obj.name)
+
+// 1 & 1 => 1
+// 1 & 0 => 0
+// 0 & 1 => 0
+// 0 & 0 => 0
+
+// 1 | 1 => 1
+// 1 | 0 => 1
+// 0 | 1 => 1
+// 0 | 0 => 0
+
+// #12AFBC
+// var x = { name: 'Ali' }
+// // #24ACBF
+// var y = { name: 'Ali' }
+
+// var z = y
+
+// console.log(x.name == y.name)
+
+// console.log(z == y)
+
+// var x = ('b' + 'a' + +'a' + 'a').toLowerCase()
+
+// console.log(x)
+
+// var x = 2
+
+// var y = 2 ** ++x
+
+// console.log(y)
+
+// console.log(x)
+
+// var x = 10
+
+// x = x + 1
+
+// console.log(x)
+
+// x += 1
+
+// console.log(x)
+
+// x -=- 1
+
+// console.log(x)
+
+// x = x - -1
+
+// console.log(14)
+
+// // #12ABF5
+// var p1 = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi'
+// }
+
+// // #12ABF5
+// var p2 = p1
+
+// console.log(p1)
+// console.log(p2)
+
+// p2.firstName = 'Qoli'
+
+// console.log(p1)
+// console.log(p2)
+
+// var arr = [10, 20, 30, 40]
+// var arr2 = ['a', 'b', 'c']
+
+// var arr3 = ['Ali', true, null, undefined, { age: 33 }, ['a', 'b']]
+
+// console.log(arr)
+// console.log(arr2)
+// console.log(arr3)
+
+// console.log(arr3.length)
+
+// var friends = ['Ali', 'Eli', 'Qoli']
+
+// console.log(typeof friends) // object
+
+// console.log(friends.length)
+// console.log(friends[0])
+// console.log(friends[1])
+// console.log(friends[2])
+// console.log(friends[3]) // undefined
+
+// // #12AFB4
+// var person = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   age: 33,
+//   // # AB24FC
+//   family: {
+//     father: 'Mohammad',
+//     mother: 'Hajar',
+//     sisters: ['Fatemeh', 'Zahra', 'Zeinab'],
+//     brothers: ['Mostafa'],
+//     children: ['Mahsa']
+//   }
+// }
+
+// console.log(person.family.father)
+// console.log(person['family']['mother'])
+// console.log(person['family'].children[0])
+
+// console.log(typeof person.family.father)
+
+// person.gender = true
+// // person["gender"] = true
+
+// console.log(person)
+
+// delete person.age
+
+// console.log(person)
+
+// // #12AF2B
+// var ali = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   age: 33,
+//   20: 'qoli'
+// }
+
+// var propName = 'firstName'
+
+// console.log(ali[propName])
+// console.log(ali.propName) // Wrong
+
+// console.log(ali.age)
+
+// console.log(ali.age)
+// console.log(ali['firstName'])
+// console.log(ali.gender)
+// console.log(ali[20])
+
+// console.time('bigint')
+
+// // console.log(222n ** 22n)
+// console.log(222 ** 22)
+
+// console.timeEnd('bigint')
+
+// console.time('number')
+
+// console.timeEnd('number')
+
+// int number = 10
+// number  = "Ali"
+
+// var firstName = 'Ali'
+
+// console.log(firstName)
+
+// var lastName = 'Mousavi',
+//   age = 33
+
+// var نام = "علی"
+
+// console.log('JavaScript')
+
+/*
+
+console.log('External JS file', 'ali', 15)
+
+console.log('Another log')
+
+*/
+
+// console.log('Ali')
