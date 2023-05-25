981f84736988ead5ba719126061418fcf2fdfe86
setali <set.ali.mousavi@gmail.com>
2023-05-25 13:56:41 +0330

    Session 4

---
+++ exercise.js
@@ -0,0 +1,78 @@
+//  00011010101010101
+
+// 00000000000000000
+
+//////////4
+
+// var alireza = {
+//   firstName: 'Alireza',
+//   lastName: 'Jamali',
+//   birthYear: '1377',
+//   bornCity: 'Busher',
+//   job: 'JavaScript forntend developer',
+//   familyMembers: {
+//     parents: {
+//       father: {
+//         firstName: 'Bahram',
+//         lastName: 'Jamali',
+//         birthYear: '1341',
+//         bornCity: 'Shiraz',
+//         job: 'Retired'
+//       },
+//       mother: {
+//         firstName: 'Mahnaz',
+//         lastName: 'Ramezan zadeh',
+//         birthYear: '1343',
+//         bornCity: 'Abadan',
+//         job: 'Housekeeper'
+//       }
+//     },
+//     sibilings: {
+//       sisters: [
+//         {
+//           firstName: 'Maryam',
+//           lastName: 'Jamali',
+//           birthYear: '1370',
+//           bornCity: 'Bushehr',
+//           job: 'English Teacher'
+//         }
+//       ],
+//       brothers: []
+//     }
+//   },
+//   friends: {
+//     regularFriends: ['Hossein', 'Mehdi', 'Ayeh', 'Amin', 'Kazhal'],
+//     bestFriends: ['Amir Mohammad', 'Negin', 'Alireza', 'Ali']
+//   }
+// }
+
+// var ali = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   age: 33,
+//   family: {
+//     father: 'Mohammad',
+//     mother: 'Hajar'
+//   },
+//   friends: ['Ali', 'Qoli', 'Eli']
+// }
+
+// console.log(ali)
+///// 1
+
+// var name = 'Ali',
+//   family = 'Mousavi',
+//   city = 'Shiraz',
+//   age = 33
+
+// console.log(
+//   'My name is ' +
+//     name +
+//     ' ' +
+//     family +
+//     "\n\t=> I'm living in " +
+//     city +
+//     '\n\t=> I have ' +
+//     age +
+//     ' years old'
+// )
--- index.js
+++ index.js
@@ -1,185 +1,271 @@
-//  ! @ # $ % ^ & * () _ - + = \ / | [] {} ~ ` ' " : ; ? . ,
+// function func () {}
 
+// ;[a, b] = [2, 4]
 
+// ;(function () {})()
+
+// // Mongoose.findAll(function (data) {
+// //   return response(data)
+// // })
+
+// function sum (a, b) {
+//   return a + b
+// }
+
+// function sub (a, b) {
+//   return a - b
+// }
+
+// var noop = function () {}
+// const noop = () => {}
+
+// function execute (a, b, cb = noop) {
+//   //   console.log(a, b, cb)
+//   //   console.log(typeof action)
+//   return cb(a, b)
+// }
+
+// var result = execute(4, 2)
+
+// console.log(result)
+
+// console.log(
+//   execute(8, 6, function (a, b) {
+//     return a * b
+//   })
+// )
+
+// var result = execute(14, 2, function (a, b) {
+//   return a / b
+// })
+
+// console.log(result)
+
+// console.log(execute(8, 6, sum))
+// console.log(execute(8, 6, sub))
+
+// function execute (a, b, action) {
+//   if (action === 'sum') {
+//     return sum(a, b)
+//   } else if (action === 'sub') {
+//     return sub(a, b)
+//   }
+// }
+
+// console.log(execute(8, 6, 'sub'))
+
+// console.log(sum(8, 6))
+// console.log(sub(8, 6))
+
+// var sum = function (a, b) {
+//   return a + b
+// }
+
+// console.log(sum(2, 5))
+
+// function isOdd (num) {
+//   return !!(num % 2)
+// }
+
+// function isEven (num) {
+//   return !isOdd(num)
+// }
+
+// console.log(isOdd(11))
+
+// console.log(isEven(13))
+
+// function evenOrOdd (num) {
+//   return typeof num === 'number' ? (num % 2 ? 'odd' : 'even') : 'Not a number'
+// }
+
+// console.log(evenOrOdd(12))
+// console.log(evenOrOdd(187))
+// console.log(evenOrOdd('ali'))
+
+// function sayHello () {
+//   console.log('Hello')
+
+//   return
+
+//   console.log('salam') // not execute
+//   console.log('qolam') // not execute
+// }
+
+// var result = sayHello()
+
+// console.log(result)
+
+// sayHello()
+// sayHello()
+// sayHello()
+// sayHello()
+// sayHello()
+
+// function sum (a = 0, b = 0, c = 0) {
+//   //   if (c === undefined) {
+//   //     c = 0
+//   //   }
+
+//   //   c = c === undefined ? 0 : c
+
+//   //   c = c ?? 0
+
+//   //   console.log(a, b)
+//   var result = a + b + c
+
+//   return result
+// }
+
+// console.log(sum(2, 5))
+// console.log(sum(2, 5, 10))
+// console.log(sum(2))
+// console.log(sum())
+
+// console.log(sum(2, 7))
+// console.log(sum(20, 17))
+// console.log(sum(22, 6))
+// console.log(sum(22, 6, 5))
+
+// var person = {
+//   name: 'hosein',
+//   family: 'hosein',
+//   age: 30
+// }
+
+// console.log('name' in person)
+
+// for (var key in person) {
+//   console.log(key, ' => ', person[key])
+// }
+
+// for (var char of 'salam') {
+//   console.log(char)
+// }
+
+// var friends = ['Ali', 'Eli', 'Qoli', 'Fati', 'Mahsa']
+
+// for (var friend of friends) {
+//   console.log(friend)
+// }
+
+// var friends = [
+//   { name: 'ahmad', gender: 'male' },
+//   { name: 'hanieh', gender: 'female' },
+//   { name: 'kasra', gender: 'male' },
+//   { name: 'leila', gender: 'female' }
+// ]
+
+// for (var i = 0; i < friends.length; i++) {
+//   if (friends[i].gender === 'female') {
+//     console.log(friends[i].name)
+//   }
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   if (i % 2) {
+//     continue
+//   }
+
+//   console.log(i)
+// }
 
 // var obj = {
-//   name: 'Ali',
-//   family: 'Mousavi',
-//   fullName: 'Ali Mousavi'
+//     name: 'Ali',
+//     completeProfile: false
+// }
+
+// obj.completeProfile ? redirectToHome() : reDirectToProfile()
+
+// var friends = ['Ali', 'Eli', 'Qoli', 'Fati', 'Mahsa']
+
+// for (var i = 0; i < friends.length; i++) {
+//   console.log(friends[i])
+// }
+
+// var str = 'salam'
+
+// for (var i = 0; i < str.length; i++) {
+//   console.log(str[i])
+// }
+
+// var a = 10
+
+// do {
+//   console.log(a)
+//   a++
+// } while (a < 10)
+
+// var a = 10
+
+// while (a < 10) {
+//   console.log(a)
+//   a++
+// }
+
+// var i = 0
+
+// for (;;) {
+//   console.log('salam', i)
+//   i++
+//   if (i >= 10) {
+//     break
+//   }
+// }
+
+// var obj = {
+//   name: 'Ali'
+//   //   family: 'Mousavi'
+//   //   fullName: 'Ali Mousavi'
 // }
 
 // console.log(obj.fullName || obj.family || obj.name)
 
-// 1 & 1 => 1
-// 1 & 0 => 0
-// 0 & 1 => 0
-// 0 & 0 => 0
+// var value = 10
 
-// 1 | 1 => 1
-// 1 | 0 => 1
-// 0 | 1 => 1
-// 0 | 0 => 0
+// value % 2 ? console.log('Odd') : console.log('Even')
 
-// #12AFBC
-// var x = { name: 'Ali' }
-// // #24ACBF
-// var y = { name: 'Ali' }
+// console.log(value % 2 ? 'Odd' : 'Even')
 
-// var z = y
+// var value = 'f'
 
-// console.log(x.name == y.name)
+// switch (value) {
+//   case 'A':
+//   case 'a':
+//     console.log('Value is A')
+//     break
 
-// console.log(z == y)
+//   case 'B':
+//   case 'b':
+//     console.log('Value is B')
+//     break
 
-// var x = ('b' + 'a' + +'a' + 'a').toLowerCase()
+//   case 'C':
+//   case 'c':
+//     console.log('Value is C')
+//     break
 
-// console.log(x)
-
-// var x = 2
-
-// var y = 2 ** ++x
-
-// console.log(y)
-
-// console.log(x)
-
-// var x = 10
-
-// x = x + 1
-
-// console.log(x)
-
-// x += 1
-
-// console.log(x)
-
-// x -=- 1
-
-// console.log(x)
-
-// x = x - -1
-
-// console.log(14)
-
-// // #12ABF5
-// var p1 = {
-//   firstName: 'Ali',
-//   lastName: 'Mousavi'
+//   default:
+//     console.log('Value is not A, B, or C')
 // }
 
-// // #12ABF5
-// var p2 = p1
+// if (heavy condition) {
+// if (true) console.log('Ali') // not good
 
-// console.log(p1)
-// console.log(p2)
+// var a = 24
 
-// p2.firstName = 'Qoli'
-
-// console.log(p1)
-// console.log(p2)
-
-// var arr = [10, 20, 30, 40]
-// var arr2 = ['a', 'b', 'c']
-
-// var arr3 = ['Ali', true, null, undefined, { age: 33 }, ['a', 'b']]
-
-// console.log(arr)
-// console.log(arr2)
-// console.log(arr3)
-
-// console.log(arr3.length)
-
-// var friends = ['Ali', 'Eli', 'Qoli']
-
-// console.log(typeof friends) // object
-
-// console.log(friends.length)
-// console.log(friends[0])
-// console.log(friends[1])
-// console.log(friends[2])
-// console.log(friends[3]) // undefined
-
-// // #12AFB4
-// var person = {
-//   firstName: 'Ali',
-//   lastName: 'Mousavi',
-//   age: 33,
-//   // # AB24FC
-//   family: {
-//     father: 'Mohammad',
-//     mother: 'Hajar',
-//     sisters: ['Fatemeh', 'Zahra', 'Zeinab'],
-//     brothers: ['Mostafa'],
-//     children: ['Mahsa']
-//   }
+// if (a % 2) {
+//   console.log('Odd')
+// } else {
+//   console.log('Even')
 // }
 
-// console.log(person.family.father)
-// console.log(person['family']['mother'])
-// console.log(person['family'].children[0])
+// var a = 10
 
-// console.log(typeof person.family.father)
-
-// person.gender = true
-// // person["gender"] = true
-
-// console.log(person)
-
-// delete person.age
-
-// console.log(person)
-
-// // #12AF2B
-// var ali = {
-//   firstName: 'Ali',
-//   lastName: 'Mousavi',
-//   age: 33,
-//   20: 'qoli'
+// if (a > 10) {
+//   console.log('A is greater than 10')
+// } else if (a < 10) {
+//   console.log('A is less than 10')
+// } else {
+//   console.log('A is equal 10')
 // }
-
-// var propName = 'firstName'
-
-// console.log(ali[propName])
-// console.log(ali.propName) // Wrong
-
-// console.log(ali.age)
-
-// console.log(ali.age)
-// console.log(ali['firstName'])
-// console.log(ali.gender)
-// console.log(ali[20])
-
-// console.time('bigint')
-
-// // console.log(222n ** 22n)
-// console.log(222 ** 22)
-
-// console.timeEnd('bigint')
-
-// console.time('number')
-
-// console.timeEnd('number')
-
-// int number = 10
-// number  = "Ali"
-
-// var firstName = 'Ali'
-
-// console.log(firstName)
-
-// var lastName = 'Mousavi',
-//   age = 33
-
-// var نام = "علی"
-
-// console.log('JavaScript')
-
-/*
-
-console.log('External JS file', 'ali', 15)
-
-console.log('Another log')
-
-*/
-
-// console.log('Ali')
