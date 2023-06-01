7e5dc561ccd787dd6cb59be1fecdd822c11e7b74
setali <set.ali.mousavi@gmail.com>
2023-06-01 13:12:02 +0330

    Session 5

--- index.js
+++ index.js
@@ -1,271 +1,256 @@
-// function func () {}
+// Pass by reference
+// #ADF214
+var a = { value: 10 }
 
-// ;[a, b] = [2, 4]
+function func (b) {
+  b.value++
+  console.log(b)
+}
 
-// ;(function () {})()
+func(a)
 
-// // Mongoose.findAll(function (data) {
-// //   return response(data)
-// // })
+console.log(a)
+// // Pass by value
+// var a = 20
 
-// function sum (a, b) {
-//   return a + b
+// function func (b) {
+//   b++
+//   console.log(b)
 // }
 
-// function sub (a, b) {
-//   return a - b
+// func(a)
+
+// console.log(a)
+
+// function useState (defaultValue) {
+//   var value = defaultValue
+
+//   function setValue (newValue) {
+//     value = newValue
+//     console.log(value)
+//   }
+
+//   return [value, setValue]
 // }
 
-// var noop = function () {}
-// const noop = () => {}
+// var arr = useState(10)
 
-// function execute (a, b, cb = noop) {
-//   //   console.log(a, b, cb)
-//   //   console.log(typeof action)
-//   return cb(a, b)
-// }
+// var value = arr[0]
+// console.log(value)
 
-// var result = execute(4, 2)
+// var setValue = arr[1]
 
-// console.log(result)
+// setValue(20)
 
-// console.log(
-//   execute(8, 6, function (a, b) {
-//     return a * b
-//   })
-// )
+// setValue(30)
 
-// var result = execute(14, 2, function (a, b) {
-//   return a / b
-// })
+// var counter1 = (function () {
+//   var counter = 1
 
-// console.log(result)
+//   function increase () {
+//     counter++
+//     console.log(counter)
+//   }
 
-// console.log(execute(8, 6, sum))
-// console.log(execute(8, 6, sub))
+//   function decrease () {
+//     counter--
+//     console.log(counter)
+//   }
 
-// function execute (a, b, action) {
-//   if (action === 'sum') {
-//     return sum(a, b)
-//   } else if (action === 'sub') {
-//     return sub(a, b)
+//   return {
+//     increase,
+//     decrease
+//   }
+// })()
+
+// console.log(counter1)
+
+// var counter1 = Counter()
+
+// console.log(counter1)
+
+// function func (a) {
+//   return function (b) {
+//     return function (c) {
+//       return a + b + c
+//     }
 //   }
 // }
 
-// console.log(execute(8, 6, 'sub'))
+// var sum5 = func(5)
 
-// console.log(sum(8, 6))
-// console.log(sub(8, 6))
+// console.log(sum5)
 
-// var sum = function (a, b) {
-//   return a + b
+// var sum20 = sum5(15)
+
+// console.log(sum20)
+
+// console.log(sum20(5))
+// console.log(sum20(15))
+// console.log(sum20(1))
+
+// console.log(sum5(6)(4))
+// console.log(sum5(16)(4))
+
+// console.log(func(2)(6)(10))
+
+// console.log(func(5)(7)(9))
+
+// function func () {
+//   // #AD21FA
+//   var i
+//   for (i = 0; i < 5; i++) {
+//     setTimeout(function () {
+//       console.log(i)
+//     }, 1000)
+//   }
 // }
 
-// console.log(sum(2, 5))
+// func()
 
-// function isOdd (num) {
-//   return !!(num % 2)
+// var i = 1
+
+// var intervalId = setInterval(function () {
+//   console.log(i++)
+// }, 1000)
+
+// console.log('id', intervalId)
+
+// setTimeout(function () {
+//   clearInterval(intervalId)
+// }, 10000)
+
+// console.log(1)
+
+// setTimeout(function () {
+//   console.log('salam')
+// }, 1000)
+
+// console.log(2)
+
+// setTimeout(function () {
+//   console.log('ali')
+// }, 2000)
+
+// var a = 10
+
+// var a = 20
+
+// console.log(a)
+
+// func()
+
+// function func () {
+//   console.log('salam')
 // }
 
-// function isEven (num) {
-//   return !isOdd(num)
+// var a
+
+// console.log(a)
+
+// a = 20
+
+// console.log((a = 20))
+
+// var a = 10
+// window.a3 = 40
+
+// function func () {
+//   a1 = 20
+//   window.a2 = 30
 // }
 
-// console.log(isOdd(11))
+// func()
 
-// console.log(isEven(13))
+// console.log(a1)
+// console.log(a2)
 
-// function evenOrOdd (num) {
-//   return typeof num === 'number' ? (num % 2 ? 'odd' : 'even') : 'Not a number'
+// var a = 20
+
+// window.alert('salam')
+
+// console.log(a)
+// console.log(window.a)
+// // #21DFA1
+// var a = 10
+
+// function func () {
+//   var a = 20
+
+//   console.log(a)
+
+//   function func2 () {
+//     console.log(a)
+//     var b = 12
+//   }
+
+//   func2()
+
+//   console.log(b)
 // }
 
-// console.log(evenOrOdd(12))
-// console.log(evenOrOdd(187))
-// console.log(evenOrOdd('ali'))
+// func()
 
-// function sayHello () {
-//   console.log('Hello')
+// console.log(a)
 
-//   return
-
-//   console.log('salam') // not execute
-//   console.log('qolam') // not execute
+// function factorial (n) {
+//   return n === 1 ? 1 : n * factorial(n - 1)
 // }
 
-// var result = sayHello()
+// console.log(factorial(5))
 
-// console.log(result)
+// 5! = 5 * 4!
+// 4! = 4 * 3!
+// 3! = 3 * 2!
+// 2! = 2 * 1!
+// 1! = 1
 
-// sayHello()
-// sayHello()
-// sayHello()
-// sayHello()
-// sayHello()
+// function factorial (n) {
+//   var result = n
 
-// function sum (a = 0, b = 0, c = 0) {
-//   //   if (c === undefined) {
-//   //     c = 0
-//   //   }
-
-//   //   c = c === undefined ? 0 : c
-
-//   //   c = c ?? 0
-
-//   //   console.log(a, b)
-//   var result = a + b + c
+//   for (var i = n - 1; i > 1; i--) {
+//     result *= i
+//   }
 
 //   return result
 // }
 
-// console.log(sum(2, 5))
-// console.log(sum(2, 5, 10))
-// console.log(sum(2))
+// console.log(factorial(5))
+
+// Stack => Last In First Out => LIFO
+// Queue => First In First Out => FIFO
+
+// function func () {
+//   func()
+// }
+
+// func()
+
+// 5! = 5 * 4!
+// 4! = 4 * 3!
+// 3! = 3 * 2!
+// 2! = 2 * 1!
+// 1! = 1
+
+// 5! = 5 * 4 * 3 * 2 * 1
+// 4! = 4 * 3 * 2 * 1
+// 3! = 3 * 2 * 1
+// 2! = 2 * 1
+// 1! = 1
+
+// function sum () {
+//   console.log(arguments)
+
+//   var result = 0
+
+//   for (var i = 0; i < arguments.length; i++) {
+//     result = result + arguments[i]
+//   }
+
+//   return result
+// }
+
+// // console.log(sum(5, 7))
+// // console.log(sum(5, 7, 41))
+// // console.log(sum(5, 7, 41, 4, 5, 8))
 // console.log(sum())
-
-// console.log(sum(2, 7))
-// console.log(sum(20, 17))
-// console.log(sum(22, 6))
-// console.log(sum(22, 6, 5))
-
-// var person = {
-//   name: 'hosein',
-//   family: 'hosein',
-//   age: 30
-// }
-
-// console.log('name' in person)
-
-// for (var key in person) {
-//   console.log(key, ' => ', person[key])
-// }
-
-// for (var char of 'salam') {
-//   console.log(char)
-// }
-
-// var friends = ['Ali', 'Eli', 'Qoli', 'Fati', 'Mahsa']
-
-// for (var friend of friends) {
-//   console.log(friend)
-// }
-
-// var friends = [
-//   { name: 'ahmad', gender: 'male' },
-//   { name: 'hanieh', gender: 'female' },
-//   { name: 'kasra', gender: 'male' },
-//   { name: 'leila', gender: 'female' }
-// ]
-
-// for (var i = 0; i < friends.length; i++) {
-//   if (friends[i].gender === 'female') {
-//     console.log(friends[i].name)
-//   }
-// }
-
-// for (var i = 0; i < 10; i++) {
-//   if (i % 2) {
-//     continue
-//   }
-
-//   console.log(i)
-// }
-
-// var obj = {
-//     name: 'Ali',
-//     completeProfile: false
-// }
-
-// obj.completeProfile ? redirectToHome() : reDirectToProfile()
-
-// var friends = ['Ali', 'Eli', 'Qoli', 'Fati', 'Mahsa']
-
-// for (var i = 0; i < friends.length; i++) {
-//   console.log(friends[i])
-// }
-
-// var str = 'salam'
-
-// for (var i = 0; i < str.length; i++) {
-//   console.log(str[i])
-// }
-
-// var a = 10
-
-// do {
-//   console.log(a)
-//   a++
-// } while (a < 10)
-
-// var a = 10
-
-// while (a < 10) {
-//   console.log(a)
-//   a++
-// }
-
-// var i = 0
-
-// for (;;) {
-//   console.log('salam', i)
-//   i++
-//   if (i >= 10) {
-//     break
-//   }
-// }
-
-// var obj = {
-//   name: 'Ali'
-//   //   family: 'Mousavi'
-//   //   fullName: 'Ali Mousavi'
-// }
-
-// console.log(obj.fullName || obj.family || obj.name)
-
-// var value = 10
-
-// value % 2 ? console.log('Odd') : console.log('Even')
-
-// console.log(value % 2 ? 'Odd' : 'Even')
-
-// var value = 'f'
-
-// switch (value) {
-//   case 'A':
-//   case 'a':
-//     console.log('Value is A')
-//     break
-
-//   case 'B':
-//   case 'b':
-//     console.log('Value is B')
-//     break
-
-//   case 'C':
-//   case 'c':
-//     console.log('Value is C')
-//     break
-
-//   default:
-//     console.log('Value is not A, B, or C')
-// }
-
-// if (heavy condition) {
-// if (true) console.log('Ali') // not good
-
-// var a = 24
-
-// if (a % 2) {
-//   console.log('Odd')
-// } else {
-//   console.log('Even')
-// }
-
-// var a = 10
-
-// if (a > 10) {
-//   console.log('A is greater than 10')
-// } else if (a < 10) {
-//   console.log('A is less than 10')
-// } else {
-//   console.log('A is equal 10')
-// }
