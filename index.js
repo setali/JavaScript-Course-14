2c51cb15dd0470042e72ae3c97ded1e18b465cfb
setali <set.ali.mousavi@gmail.com>
2023-07-06 14:04:10 +0330

    Session 9

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-function handleClick () {
-  console.log('Clicked')
+// var url = 'https://jsonplaceholder.ir/users/1'
+var url = 'http://localhost:3000/'
+var placeHolder = document.getElementById('data')
+
+var xhr = new XMLHttpRequest()
+
+function getData (url, method = 'GET') {
+  xhr.onreadystatechange = function () {
+    console.log(xhr.readyState)
+  }
+  xhr.open(method, url)
+
+  xhr.send()
+
+  xhr.onload = function () {
+    var data = JSON.parse(xhr.response)
+
+    placeHolder.textContent = data.name
+  }
+
+  xhr.onerror = function () {
+    console.log('Error')
+  }
 }
 
-function handleMouseOver () {
-  console.log('Mouse Over')
-}
+document.getElementById('action').addEventListener('click', function () {
+  getData(url)
+})
+
+// function setCookie (name, value, time) {
+//   var date = new Date()
+
+//   date.setSeconds(date.getSeconds() + time)
+
+//   document.cookie =
+//     name + '=' + value + ';expires=' + date.toUTCString() + ';path=/'
+// }
+
+// function deleteCookie (name) {
+//   setCookie(name, '', 0)
+// }
+
+// function getCookie (name) {
+//   return Object.fromEntries(
+//     document.cookie.split('; ').map(function (el) {
+//       return el.split('=')
+//     })
+//   )[name]
+// }
+
+// var Cookies = {
+//   set: setCookie,
+//   get: getCookie,
+//   remove: deleteCookie
+// }
+
+// console.log(Cookies.get('name'))
